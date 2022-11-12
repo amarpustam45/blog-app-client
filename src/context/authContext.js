874d90dc83ba95f6f2fd39786afd05262fe9a7fb
amarpustam45 <amarpustam@hotmail.com>
2022-11-12 14:44:31 -0400

    updated front end

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -10,7 +10,9 @@ export const AuthContextProvider = ({ children }) => {
   );
 
   const login = async (inputs) => {
-    const res = await axios.post(`${API_URL}/auth/login`, inputs);
+    const res = await axios.post(`${API_URL}/auth/login`, inputs, {
+      withCredentials: true,
+    });
     setCurrentUser(res.data);
   };
 
